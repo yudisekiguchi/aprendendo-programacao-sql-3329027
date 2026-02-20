de6f39c5f58bf2bb9fdfbc0333da194fa6cc65be
Yudi Sekiguchi <62259062+yudisekiguchi@users.noreply.github.com>
2026-02-20 22:49:27 +0000

    adiciona coluna 'nivel' à tabela salario_classe com tipo varchar(50)

--- exercicios/1-CRUD/2-alterar-tabelas.sql
+++ exercicios/1-CRUD/2-alterar-tabelas.sql
@@ -1,3 +1,4 @@
 -- Na tabela salario_classe altere o atributo "nivel" para que seu tipo de dado seja varchar, com tamanho máximo de 50 caracteres
-
+ALTER TABLE salario_classe
+ADD nivel VARCHAR(50);
 -- Na tabela employees, adicione uma coluna para receber o id_salario
